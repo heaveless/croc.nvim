953112a8b1a71705de185f6bd8ea0c57d9352a45
Alberto Llamocca <me@heaveless.com>
2023-10-24 18:51:44 -0500

    add commands

--- lua/croc/init.lua
+++ lua/croc/init.lua
@@ -1,7 +1,32 @@
+local api = vim.api
 local M = {}
 
 function M.setup(config)
-  print "config croc is running..."
+  api.nvim_create_user_command(
+    "CrocSend", function(args)
+      print "config croc CrocSend is running..."
+    end,
+    {
+      range = true,
+      nargs = '?',
+      complete = function(ArgLead, CmdLine, CursorPos)
+        return {}
+      end
+    }
+  )
+
+  api.nvim_create_user_command(
+    "CrocGet", function(args)
+      print "config croc CrocGet is running..."
+    end,
+    {
+      range = true,
+      nargs = '?',
+      complete = function(ArgLead, CmdLine, CursorPos)
+        return {}
+      end
+    }
+  )
 end
 
 return M
